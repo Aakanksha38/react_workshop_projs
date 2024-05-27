baa6d9c6d5f79c655fa2b4cb04ecc53ac2963565
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-05-28 00:55:12 +0530

    Birthday reminder - concepts(useSate)

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [person, setPerson] = useState(data);
+  //passing person as prop to list component ,, and in list component we can distructure 
+  return (
+    <main>
+      <section className='container'>
+        <h3>{person.length} Birthday's today</h3>
+        <List people={person}></List>
+        <button className='btn' onClick={()=>{setPerson([])}}> clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
