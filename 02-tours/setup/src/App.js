a50a1827401dc0aa9cf736a43e34a7051a53a1fc
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-09-06 01:15:31 +0530

    proj 2 , based on state, useeffect, conditional rendering and api calling

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -3,9 +3,60 @@ import Loading from './Loading'
 import Tours from './Tours'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = 'https://www.course-api.com/react-tours-project'; //this url is working fine 
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading , setLoading ] = useState(true);
+  const [tours , setTours] = useState([]);
+  const removeTour =(id)=>{
+    const newTours = tours.filter((tour)=>tour.id !==id) 
+    setTours(newTours);
+  }
+
+  const removeAllTourIds = ()=>{
+    setTours([]);
+  }
+
+  const fetchUrl =async () =>{
+    setLoading(true); // coz while fetching the data,i dont have any data to show so we want to laoding to be true 
+    try{
+      const response = await fetch(url);
+      const tour = await response.json();
+      setLoading(false);
+      setTours(tour);
+    } catch (error){
+      setLoading(false);
+      console.log(error);
+      
+    }
+  }
+
+  useEffect(()=>{
+    fetchUrl();
+  },[])
+  
+  if(loading){
+   return <Loading/>
+  }
+
+  //when we remove all the tourids , thn nothing will be left , so at that time when 
+  //clicked on some btn , thn all my tours should get shown again 
+  if(tours.length === 0){
+    return(
+      <main>
+        <div className='title'>
+          <h2>no tours left</h2>
+          <button className='btn' onClick={()=>fetchUrl()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} removeAllTourIds={removeAllTourIds}/>
+    </main>
+  )
 }
 
 export default App
--- 02-tours/setup/src/Tour.js
+++ 02-tours/setup/src/Tour.js
@@ -1,7 +1,34 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, name,info,image,price,removeTourId}) => {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className='tour-price'>${price}</h4>
+        </div>
+        <p>{readMore ? info : `${info.substring(0,200)}...`}</p>
+        <button onClick={()=>{setReadMore(!readMore)}}>{readMore ? "show Less" : "show More"}</button>
+        <button className='delete-btn' onClick={()=> removeTourId(id)}>not interested</button>
+      </footer>
+      
+    </article>
+  )
 };
 
 export default Tour;
+
+/*
+<p>{readMore ? info : `${info.substring(0,200)}...`}</p>
+        <button onClick={()=>{setReadMore(!readMore)}}>{readMore ? "show Less" : "show More"}</button>
+ here , what we are trying to do is :
+ readmore is flase by default thn we r trimming the long para into shorter 
+ n we want that to read whole long thn , we given btn - when clicked long n shorter para will be displayed        
+
+
+
+
+        */
