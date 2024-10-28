7f623f0d5024a1420ce93da097b728992137fefa
Aakanksha38 <chippa.aakanksha.16ce1023@gmail.com>
2024-10-28 17:11:09 +0530

    proj-08 color generator based on react form,useEffect,useState,external lib-values.js

--- 08-color-generator/setup/src/App.js
+++ 08-color-generator/setup/src/App.js
@@ -1,10 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState , useEffect } from 'react'
 import SingleColor from './SingleColor'
 
-import Values from 'values.js'
+import Values from 'values.js' //values.js is a lib which we will use for color shades , we need to import this , link to this is in README.md
+/**
+ * useState for i/p color , list of colors to show , error - in case user enter invalid color code 
+ * 2 sections - 1 section for form - label input text, btn n other section for list of color shades to display
+ * using values.js lib to show colors  ===    let colors = new Values(color).all(10);
+ * try ctach block so to catch the error if user gives incorrect color-code
+ * when user give incorrect color code - user should get to know what mistake made , so i/p box should turn into red in case of error
+ * {listOfShades.map((color,index)=>{
+          return <SingleColor key={index} {...color} index={index}></SingleColor>
+        })}  =>>> all the infor we r getting in color ,passing it as prop with spred operator(...)
+  * now , here initally when we go to screen there is no color shade appears , but list should have any default code value so to display color-shade on screen ==>> assign a color-code to list 
 
+ * **/
+ 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState('');
+  const [listOfShades, setListOfShades] = useState(new Values('#ffd700').all(10));
+  const [error, setError] = useState(false);
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      console.log(colors);
+      setListOfShades(colors); //list which is we shown in console now present in color
+      setError(false);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+    //let colors = new Values(color).all(10); // grab the color whatever we are passing n get all shades of it (color).all(10)
+    //console.log(colors);
+  };
+  
+  return(
+    <>
+      <section className='container'>
+        <h3>Color Generator:</h3>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            name="color" 
+            id="color" 
+            placeholder='#ffd700' 
+            value={color} 
+            onChange={(e)=> 
+            setColor(e.target.value)}
+            className={`${error? "error" : null}`}
+            />
+          <button 
+            type="submit" 
+            className="btn">
+              Generate
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+      {listOfShades.map((color,index)=>{
+          return <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+        })}
+      </section>
+
+    </>
+  );
 }
 
 export default App
--- 08-color-generator/setup/src/SingleColor.js
+++ 08-color-generator/setup/src/SingleColor.js
@@ -1,8 +1,39 @@
 import React, { useState, useEffect } from 'react'
 import rgbToHex from './utils'
-
-const SingleColor = () => {
-  return <h4>single color</h4>
+/**
+ * when we console the color , we got rgb, weight,type,hex
+ * with rgb = we show colour
+ * with weight = we show % of that color
+ * in rgb , we have 3 elements , 0,1,2- by combining/joining all we can make a color which can actually display on screen
+ * to copy the color-code - we want onclick, oncick on whole block n u can copy the code 
+ * copied to clipboard should be there for only few secs, wht to do? useeffect =>based on state change we want to show n hide , simetimeout= should be there for few secs
+ * now , here initally when we go to screen no color shade appears , but there should be any default color shade to be displayed on screen ==>>
+ * **/
+const SingleColor = ({hexColor, rgb, alpha, type, weight}) => {
+  const [alert , setAlert] = useState(false); // for alert = copied to clipboard
+  const bcg = rgb.join(",");
+  //console.log(hexColor);
+  const hexValue = `#${hexColor}` ;
+  useEffect(()=>{
+    const timeOut = setTimeout(()=>{
+      setAlert(false);
+    },2000);
+    return () => clearTimeout(timeOut);
+  },[alert]);
+  return (
+    <article
+      className={`color`}
+      style= {{backgroundColor: `rgb(${bcg})`}}
+      onClick={()=>{
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
+    >
+      <p>{weight}%</p>
+      <p>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p> } 
+    </article>
+  );
 }
 
 export default SingleColor
